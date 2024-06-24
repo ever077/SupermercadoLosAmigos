182fc6f585ae0674d88402669e60767a3145def3
ever077 <everman77@gmail.com>
2024-06-23 21:20:42 -0300

    Se desarrollo el controlador de categorias que se conecta a la API REST para cargar los productos segun la categoria seleccionada

---
+++ JS/categories/categoriesController.js
@@ -0,0 +1,191 @@
+/*
+  Controlador que se encarga de manejar las categorias e items conectandose
+  con la API REST creada con Node.js y Express.js.
+  Creado por el grupo 18 del curso "Codo a Codo 24258 - 2024"
+*/
+
+// Constante con la direccion de la API REST
+const API_URL = "http://localhost:3000/api/v1.0/category";
+
+// Constantes con los ID de las categorias
+const ID_ALMACEN = "1";
+const ID_BEBES = "2";
+const ID_HOGAR = "3";
+const ID_PERFUMERIA = "4";
+const ID_BEBIDAS = "5";
+const ID_CONGELADOS = "6";
+
+/***********        EVENT LISTENERS        ***********/
+
+// Obtener items de la categoria: Almacen
+document.getElementById("category-almacen").addEventListener("click", (event) => {
+  // Hago la peticion a la API para que me liste todas los items de la categoria
+  fetch(
+    `${API_URL}/${ID_ALMACEN}`,
+    {
+      method: 'GET',
+      headers: {
+        accept: 'application/json'
+      }
+    }
+  )
+  .then(response => response.json())
+  .then((data) => {
+    mostrarItems(data.items);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+});
+
+// Obtener items de la categoria: Bebes
+document.getElementById("category-bebes").addEventListener("click", (event) => {
+  // Hago la peticion a la API para que me liste todas los items de la categoria
+  fetch(
+    `${API_URL}/${ID_BEBES}`,
+    {
+      method: 'GET',
+      headers: {
+        accept: 'application/json'
+      }
+    }
+  )
+  .then(response => response.json())
+  .then((data) => {
+    mostrarItems(data.items);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+});
+
+// Obtener items de la categoria: Hogar
+document.getElementById("category-hogar").addEventListener("click", (event) => {
+  // Hago la peticion a la API para que me liste todas los items de la categoria
+  fetch(
+    `${API_URL}/${ID_HOGAR}`,
+    {
+      method: 'GET',
+      headers: {
+        accept: 'application/json'
+      }
+    }
+  )
+  .then(response => response.json())
+  .then((data) => {
+    mostrarItems(data.items);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+});
+
+// Obtener items de la categoria: Perfumeria
+document.getElementById("category-perfumeria").addEventListener("click", (event) => {
+  // Hago la peticion a la API para que me liste todas los items de la categoria
+  fetch(
+    `${API_URL}/${ID_PERFUMERIA}`,
+    {
+      method: 'GET',
+      headers: {
+        accept: 'application/json'
+      }
+    }
+  )
+  .then(response => response.json())
+  .then((data) => {
+    mostrarItems(data.items);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+});
+
+// Obtener items de la categoria: Bebidas
+document.getElementById("category-bebidas").addEventListener("click", (event) => {
+  // Hago la peticion a la API para que me liste todas los items de la categoria
+  fetch(
+    `${API_URL}/${ID_BEBIDAS}`,
+    {
+      method: 'GET',
+      headers: {
+        accept: 'application/json'
+      }
+    }
+  )
+  .then(response => response.json())
+  .then((data) => {
+    mostrarItems(data.items);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+});
+
+// Obtener items de la categoria: Congelados
+document.getElementById("category-congelados").addEventListener("click", (event) => {
+  // Hago la peticion a la API para que me liste todas los items de la categoria
+  fetch(
+    `${API_URL}/${ID_CONGELADOS}`,
+    {
+      method: 'GET',
+      headers: {
+        accept: 'application/json'
+      }
+    }
+  )
+  .then(response => response.json())
+  .then((data) => {
+    mostrarItems(data.items);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+});
+
+/***********        FUNCIONES        ***********/
+
+/*
+  Funcion:
+    Muestra los items de una categoria en la pantalla
+  Parametros:
+    Lista de items de una categoria
+    categoryItems [
+    {
+      "itemId": (int),
+      "categoryId": (int),
+      "name": (string),
+      "description": (string),
+      "price": (string),
+      "image": (string)
+    }
+*/
+const mostrarItems = (categoryItems) => {
+  // Verifico que haya items para mostrar
+  if (categoryItems.length > 0) {
+    // Obtengo el contenedor de las tarjetas
+    const contenedor = document.getElementById("products-container");
+
+    // Vacio el contenedor
+    while (contenedor.firstChild) {
+      contenedor.removeChild(contenedor.firstChild);
+    }
+
+    categoryItems.forEach(item => {
+      // Creo la tarjeta
+      const article = document.createElement("article");
+      article.classList.add("product");
+
+      article.innerHTML = `
+        <img src="${item.image}" alt="${item.description}">
+        <h3>${item.name}</h3>
+        <p class="description">${item.description}</p>
+        <p>Precio: $${item.price}</p>
+        <button class="btn-add-product">Agregar al carrito</button>
+      `;
+
+      // Agrego el articulo creado al contenedor
+      contenedor.appendChild(article);
+    });
+  }
+};
